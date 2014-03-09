c55309c161cc93f1ba004ad2088931c0f6a4aa5e
lat9 <lat9@vinosdefrutastropicales.com>
2014-03-09 11:50:41 -0400

    Issue #1:  Check for configuration_group_id != 0
    In both of the un-install SQL scripts, need to check that the
    superglobalsid (the configuration_group_id) is non-zero before removing
    the items, to prevent the unwanted removal of configuration group id 0.

--- superglobals_remove.sql
+++ superglobals_remove.sql
@@ -1,8 +1,4 @@
-# BOF remove old Super Globals Plus configurations
-#SET @superglobalsid=0; Don't want this for 1.5.0
-SELECT (@superglobalsid:=configuration_group_id) AS superglobalsid
-FROM configuration_group
-WHERE configuration_group_title='Superglobals';
-DELETE FROM configuration WHERE configuration_group_id=@superglobalsid;
-DELETE FROM configuration_group WHERE configuration_group_id=@superglobalsid;
+SELECT @superglobalsid:=configuration_group_id FROM configuration_group WHERE configuration_group_title="Superglobals";
+DELETE FROM configuration WHERE configuration_group_id=@superglobalsid AND @superglobalsid != 0;
+DELETE FROM configuration_group WHERE configuration_group_id=@superglobalsid AND @superglobalsid != 0;
 DELETE FROM admin_pages WHERE page_key='configSuperglobals';
