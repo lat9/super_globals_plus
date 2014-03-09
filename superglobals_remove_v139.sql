0bdd839678d2abc30856ff9edd5872a6255cd9d1
lat9 <lat9@vinosdefrutastropicales.com>
2014-03-09 11:42:47 -0400

    Revert "Issue #1: Add checks for configuration_group_id"
    This reverts commit 5d9dee1cfda80d0af2fc1da57a210b89941a3129.

--- superglobals_remove_v139.sql
+++ superglobals_remove_v139.sql
@@ -1,3 +1,9 @@
-SELECT (@superglobalsid:=configuration_group_id) AS superglobalsid FROM configuration_group WHERE configuration_group_title='Superglobals';
-DELETE FROM configuration WHERE configuration_group_id=@superglobalsid AND @superglobalsid != 0;
-DELETE FROM configuration_group WHERE configuration_group_id=@superglobalsid AND @superglobalsid != 0;
+# remove Superglobals settings
+# 2006/03/03
+
+SELECT @superglobalsid:=configuration_group_id
+FROM configuration_group WHERE configuration_group_title="Superglobals";
+
+DELETE FROM configuration WHERE configuration_group_id=@superglobalsid;
+
+DELETE FROM configuration_group WHERE configuration_group_id=@superglobalsid;
